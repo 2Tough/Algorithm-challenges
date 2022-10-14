cb436f7ba4c8b0570096b1ab653b1111832909ec
2Tough <vainferro@gmail.com>
2022-10-14 16:12:23 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let answer = document.getElementById('one')
+
+
+document.getElementById('show-answer-one').addEventListener('click', function() {
+    answer.style.display = 'block'
+})
+
+document.getElementById('hide-answer-one').addEventListener('click', function() {
+    answer.style.display = 'none'
+})
