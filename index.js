a2063a0bc35986ce6dd7f441aa6bc4f77d3ff1e0
2Tough <vainferro@gmail.com>
2022-10-14 23:32:28 +0300

    Added buttons.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 
 let answer = document.getElementById('one')
 
+let answerScreen = document.getElementsByClassName('resolved-function-screen')
+let showAnswerBtn = document.getElementsByClassName('show-answer-btn')
+let hideAnswerBtn = document.getElementsByClassName('hide-screen-btn')
 
-document.getElementById('show-answer-one').addEventListener('click', function() {
-    answer.style.display = 'block'
-})
 
-document.getElementById('hide-answer-one').addEventListener('click', function() {
-    answer.style.display = 'none'
-})
+
+
+for (let i = 0; i < showAnswerBtn.length ; i++) {
+    showAnswerBtn[i].addEventListener('click', function() {
+        answerScreen[i].style.display = 'block'  
+        hideAnswerBtn[i].style.display = 'block'  
+        
+    })
+
+
+}
+for (let j = 0; j < hideAnswerBtn.length ; j++) {
+
+hideAnswerBtn[j].addEventListener('click', function() {
+    answerScreen[j].style.display = 'none' 
+    hideAnswerBtn[j].style.display = 'none'   
+}) 
+}
